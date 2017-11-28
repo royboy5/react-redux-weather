5bca41feaf84b10333dd9e393249e3ff2d4197a7
Roy Eng <royeng@Roys-MBP.home>
2017-11-28 00:32:06 -0500

    added https to api call

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 import { FETCH_WEATHER } from './types'
 
 const API_KEY = 'd3beaf0179c14b0347f4ffbd979b7469'
-const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`
+const ROOT_URL = `https://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`
 
 export const fetchWeather = (city) => async dispatch => {
   const res = await axios.get(`${ROOT_URL}&q=${city},us`)
